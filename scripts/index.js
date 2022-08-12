33df59b8361d7fe3ce6e20b517c87896f6043985
mohab farahat <mohabfarahat007@gmail.com>
2022-08-12 21:49:21 +0200

    Added Modal and script

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,17 @@
 const orderBtn = document.getElementById("order-btn");
+const dialog = document.getElementById("dialog");
+const overlay = document.getElementById("overlay");
+// const close = document.getElementById("close");
+const submitBtn = document.getElementById("submit-order");
 
 const openOrder = () => {
   dialog.style.display = "block";
   overlay.style.display = "block";
 };
-
+function closeOrder() {
+  dialog.style.display = "none";
+  overlay.style.display = "none";
+}
 orderBtn.addEventListener("click", openOrder);
+// close.addEventListener("click", closeOrder);
+submitBtn.addEventListener("click", closeOrder);
